52648143b79aa32fbcd4208ee8b7df8e6e687e2b
Luke Hu <hyt577@gmail.com>
2018-06-20 00:02:24 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dd">
+    <ul class="list">
+      <li class="item">A</li>
+      <li class="item">B</li>
+      <li class="item">C</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+.list
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: absolute;
+  top: 1.58rem;
+  right: 0;
+  bottom: 0;
+  width: .4rem;
+  .item
+    text-align: center;
+    line-height: .44rem;
+    color: $bgColor;
+</style>

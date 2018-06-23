ddab96db10d6286b0b90bf2058d2d2b31293e9d1
Luke Hu <hyt577@gmail.com>
2018-06-23 19:37:06 +0800

    finish component

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="dd">
     <ul class="list" >
-      <li class="item" v-for="(item, key) of cities" :key="key">
-        {{key}}
+      <li class="item" v-for="item of letter" :key="item" :ref="item" @click="handleLetterClick" @touchstart="handleTouchStart" @touchmove="handleTouchMove" @touchend="handleTouchEnd">
+        {{item}}
       </li>
     </ul>
   </div>
@@ -13,6 +13,51 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  computed: {
+    letter () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  methods: {
+    handleLetterClick (ele) {
+      this.$emit('changeLetter', ele.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        if (this.touchStatus) {
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letter.length) {
+            this.$emit('changeLetter', this.letter[index])
+          }
+        }
+        this.timer = null
+      }, 16)
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,7 +17,7 @@
           </div>
         </div>
       </div>
-      <div class="area" v-for="(items, key) of cities" :key='key'>
+      <div class="area" v-for="(items, key) of cities" :key='key' :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list" v-for="item of items" :key=item.id>
           <div class="item border-bottom">{{item.name}}</div>
@@ -33,10 +33,19 @@ export default {
   name: 'CityList',
   props: {
     cities: Object,
-    hotCities: Array
+    hotCities: Array,
+    letter: String
   },
   mounted: function () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const ele = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(ele)
+      }
+    }
   }
 }
 </script>

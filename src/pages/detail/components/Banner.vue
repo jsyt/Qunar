f864aa9ec85e76560f2ffc33ebd9f49dfae3a962
Luke Hu <hyt577@gmail.com>
2018-07-01 23:15:41 +0800

    complete detail-banner

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_600x330_8572a930.jpg" alt="上海迪士尼乐园">
+    <div class="banner-info">
+      <div class="banner-title">上海迪士尼乐园</div>
+      <div class="banner-num">
+        <span class="iconfont banner-icon">&#xe62e;</span>
+        66
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang='stylus' scoped>
+ @import '~styles/varibles.styl'
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      color: #fff
+      line-height: .6rem
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+      .banner-title
+        flex: 1
+      .banner-num
+        padding: 0 .32rem
+        height: .32rem
+        padding-top: .14rem
+        line-height: .4rem
+        border-radius: .2rem
+        font-size: .24rem
+        background: raba(0, 0, 0, .8)
+        .banner-icon
+          font-size: .24rem
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import state from './state'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state,
-  mutations: {
-    changeCityMt (state, city) {
-      state.city = city
-      try {
-        localStorage.city = city
-      } catch (error) {}
-    }
-  }
+  mutations
 })
